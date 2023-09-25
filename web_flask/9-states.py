63bed0d192de7b2909d87f11df8775e9eaa3d7c8
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-09-24 21:20:57 -0700

    Fixed some bugs

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 
 if __name__ == "__main__":
@@ -13,9 +14,20 @@ if __name__ == "__main__":
     def display_states():
         """Render state_list html page to display States created"""
         states = storage.all(State)
+
         return render_template('7-states_list.html', states=states)
 
-    @app.teardown_appcontext
+    @app.route("/states/<id>", strict_slashes=False)
+    def display_state(id):
+        """Serves an HTML page populated with data of a State"""
+        states = storage.all(State)
+        cities = storage.all(City)
+        
+        for state in states:
+            if state == "States."+id:
+                return render_template("9-states.html", id=id, states=states, cities=cities)
+        return render_template("9-states.html")
+
     def teardown(self):
         """Method to remove current SQLAlchemy Session"""
         storage.close()
